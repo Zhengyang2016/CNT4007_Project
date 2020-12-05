d45db1f6048ffb58412e2b4b34eed7a4f9a31a7f
Zhengyang <lzy1300376535@gmail.com>
2020-12-05 18:29:54 -0500

    finished optimistic unchoke Scheduler

--- src/peerProcess/OptimisticScheduler.java
+++ src/peerProcess/OptimisticScheduler.java
@@ -2,10 +2,12 @@ package peerProcess;
 
 import java.util.TimerTask;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class OptimisticScheduler extends TimerTask{
 	
 	public ArrayList<Stats> connectedPeers;
+	public Stats currentOptimisticPeer;
 	
 	public OptimisticScheduler(int k, ArrayList<Stats> connectedPeers)
 	{
@@ -14,6 +16,28 @@ public class OptimisticScheduler extends TimerTask{
 	
 	public void run() 
 	{
+		ArrayList<Stats> interestedPeers = new ArrayList<Stats>();
+		for(int i = 0;i < connectedPeers.size(); ++i)
+		{
+			if(connectedPeers.get(i).interested && connectedPeers.get(i).choke)
+				interestedPeers.add(connectedPeers.get(i));
+		}
+		
+		if(currentOptimisticPeer != null)
+		{
+			currentOptimisticPeer.optimisticUnchoke = false;
+		}
+		
+		if(interestedPeers.size() > 0)
+		{
+			Random rand = new Random();
+			currentOptimisticPeer = interestedPeers.get( rand.nextInt( interestedPeers.size()) );
+			
+			new Thread( new Send(currentOptimisticPeer.out,1) ).start();
+			currentOptimisticPeer.choke = false;
+			currentOptimisticPeer.optimisticUnchoke = true;
+		}
+		
 		
 	}
 }
